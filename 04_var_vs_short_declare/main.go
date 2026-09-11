a75a502fd192604bce493d131a6b4e0edb8847a8
Ox52 <harsh04241@gmail.com>
2026-09-12 02:06:18 +0530

    -

---
+++ 04_var_vs_short_declare/main.go
@@ -0,0 +1,42 @@
+
+
+
+
+package main
+
+
+import(
+
+
+"fmt"
+
+
+
+)
+
+
+
+
+
+func main(){
+ var city string = "New York"
+
+ city = "mumbai"
+
+
+var name = "harsh"
+
+
+sub := 5000;
+
+sub = sub + 1000
+
+
+likes , comments := 100 , 50 
+
+
+fmt.Println(name, city , likes , comments , )
+
+
+
+}
