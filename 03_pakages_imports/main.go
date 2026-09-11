b0894109507c97b52fc7b70772921cedcd4baffd
Ox52 <harsh04241@gmail.com>
2026-09-12 01:55:21 +0530

    -

---
+++ 03_pakages_imports/main.go
@@ -0,0 +1,20 @@
+package main
+
+// import bring external packages into the current file, 
+import(
+
+	"fmt" // formating package for input and output
+    "math" 
+)
+
+
+
+func main(){
+
+
+
+	// packageName.fuctionName() --> call a funciton from a package
+
+
+	fmt.Println("square root of 25 " , math.Sqrt(25))
+}
