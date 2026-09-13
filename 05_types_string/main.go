9fd1b7f7e7f8cec4c5c0f3b220349997c96bbd11
Ox52 <harsh04241@gmail.com>
2026-09-13 15:11:20 +0530

    -

---
+++ 05_types_string/main.go
@@ -0,0 +1,27 @@
+
+
+package main
+
+import (
+
+	"fmt"
+	"strings"
+
+
+)
+
+
+func main(){
+
+	firstName := "harsh"
+	lastName := "harsh"
+
+	fullName := firstName + " "+ lastName
+
+
+	fmt.Println(fullName)
+
+	fmt.Println(strings.ToUpper(fullName))
+
+
+}
