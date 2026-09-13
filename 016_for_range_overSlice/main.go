8fdc1f400ee3b34aea1e811e2383c356603d3f74
Ox52 <harsh04241@gmail.com>
2026-09-13 15:11:22 +0530

    -

---
+++ 016_for_range_overSlice/main.go
@@ -0,0 +1,36 @@
+
+
+
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+
+
+	views:=  [ ]int {10,20,30,40,50}
+
+	//for range i = index , v = value
+
+	total := 0
+
+
+	for i , v := range views{
+
+		fmt.Println("day" ,i  , "views" , v)
+		total = total + v
+
+
+	}
+
+	fmt.Println(total)
+
+
+
+
+
+
+
+}
