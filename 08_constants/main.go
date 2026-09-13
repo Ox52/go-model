c610a4fdee262ed45de05f5d30833e3cdaba23ce
Ox52 <harsh04241@gmail.com>
2026-09-13 15:11:20 +0530

    -

---
+++ 08_constants/main.go
@@ -0,0 +1,24 @@
+package main
+
+
+import (
+
+	"fmt"
+)
+
+
+func main(){
+
+
+	const appName = "go basics"
+
+
+	const maxUsers int = 300
+
+	const disPrice float64 = 10.3
+
+
+	fmt.Println(appName , maxUsers , disPrice)
+
+
+}
